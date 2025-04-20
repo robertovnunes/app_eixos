0dd7d40f7a22d30e8ab987b5caa61ca504a9f74f
Roberto Nunes <jrvn@cin.ufpe.br>
2025-04-20 16:05:36 -0300

    Remove configuração de simulador de desenvolvimento do eas.json e adiciona arquivo index.ts para exportação de componentes da interface

---
+++ src/UI/components/index.ts
@@ -0,0 +1,9 @@
+import CustomDrawerContent from "./CustomDrawer";
+import FloatingButton from "./FloatingButton";
+import NewRoutine from "./NewRotine";
+import TaskItemList from "./TaskItemList";
+import { TextInputAlertGlobal } from "./TextInputAlert";
+import TimerItem from "./Timer";
+
+
+export { CustomDrawerContent, FloatingButton, NewRoutine, TaskItemList, TextInputAlertGlobal, TimerItem };
